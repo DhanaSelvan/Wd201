d79a9fb96f8cba2265f135ffd755555a265f12ac
DhanaSelvan <dhana3521@gmail.com>
2023-09-23 10:12:05 +0530

    level-7 update

--- todo-app/app.js
+++ todo-app/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 const path = require("path");
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 
